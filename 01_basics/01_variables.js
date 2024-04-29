a7d34c979fca91a801696fe5a28fc3e080e81478
Rahul raj <91648660+rahulraj0402@users.noreply.github.com>
2024-04-29 19:18:44 +0000

    learnt const , let and var

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,22 @@
+const accountId = 144553
+// this const means that it can't be changed further 
+let accountEmail = "rahul3004rt#gmail.com"
+var accountPassword = "123456789";
+accountCity = "jaipur"
+
+// accountId = 123. [ this is not allowed since we have declared the account Id as const ]
+
+accountEmail = "rohan@gmail.cpm"
+accountPassword = "98765"
+accountCity = "Dhanabd"
+
+console.table([accountCity , accountEmail , accountPassword])
+
+console.log(accountId )
+
+
+
+/*
+    const : it is constant 
+    let : 
+*/
